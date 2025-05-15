5f38465fb913115bb4dddfb8bd56138aa19c0edc
Nagashree <nagashree@clarivate.com>
2025-05-15 14:17:27 +0530

    Fix route ID  null issuein kongPatchApi function

--- src/kong-adapter/src/kong/utils.ts
+++ src/kong-adapter/src/kong/utils.ts
@@ -751,13 +751,14 @@ export function kongPatchApi(apiId: string, apiConfig: KongApi, callback: Callba
                     // so we completly replace content of the route(s) and delete whatever else left
                     for (let i = 0; i < routes.length; i += 1) {
                         routes[i].service = { id: service.id };
-                        routes[i].id = existingRoutes && existingRoutes.length > i ? existingRoutes[i].id : null;
+                        if (existingRoutes && existingRoutes.length > i) {
+                            routes[i].id = existingRoutes[i].id;
                     }
                 }
 
                 callback(null, 0, routes);
             }
-        );
+     } );
 
         // update/replace routes
         for (let i = 0; i < routes.length; i += 1) {
