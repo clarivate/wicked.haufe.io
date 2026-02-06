6779564ab971524b7fd00f8c66010f247ba8e470
Nagashree <nagashree@clarivate.com>
2026-02-06 15:22:34 +0530

    kong 3.9 update Update ACL plugin configuration to replace whitelist/blacklist with allow/deny

--- src/kong-adapter/src/kong/portal.ts
+++ src/kong-adapter/src/kong/portal.ts
@@ -579,7 +579,7 @@ function injectKeyAuth(api: ApiDescription): ApiDescription {
         name: 'acl',
         enabled: true,
         config: {
-            whitelist: [groupName]
+            allow: [groupName]
         }
     });
     return api;
@@ -661,7 +661,7 @@ function injectOAuth2Auth(api: ApiDescription): void {
         name: 'acl',
         enabled: true,
         config: {
-            whitelist: [groupName]
+            allow: [groupName]
         }
     });
 }
